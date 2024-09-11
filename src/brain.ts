9595162a6c2c09cfa8dc7c104e76524431adebcd
RetricSu <inneverland2013@gmail.com>
2024-09-11 15:42:51 +0800

    Implement tools (#1)
    * wip
    
    * fix parsing tool
    
    * chore: output tool call result
    
    * chore:

--- src/brain.ts
+++ src/brain.ts
@@ -3,13 +3,15 @@ import process from "process";
 import Readline from "readline/promises";
 import { stdOutWriteSync } from "./util";
 import { initialPrompt } from "./prompt";
-import { exec } from "child_process";
+import { spawn } from "child_process";
 import { CMessage } from "./memory/c-message";
 import { MessageView } from "./memory/message-view";
+import { buildToolCallResponseCMessage, checkIfToolCall, tools } from "./tool";
 
 export class Brain {
   public apiUrl: string;
   public ollama: Ollama;
+  public model = "llama3.1";
 
   constructor(llmApiUrl = "http://127.0.0.1:11434") {
     this.apiUrl = llmApiUrl;
@@ -17,14 +19,33 @@ export class Brain {
   }
 
   startLLMServer() {
-    exec(`OLLAMA_HOST=${this.apiUrl} ollama serve`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return;
+    return new Promise(
+      (resolve: (val: string) => any, reject: (error: string) => any) => {
+        const child = spawn("ollama", ["serve"], {
+          env: { ...process.env, OLLAMA_HOST: this.apiUrl },
+        });
+
+        // Listen to the stdout of the process
+        child.stdout.on("data", (data) => {
+          const output = data.toString();
+          resolve(output); // Resolve when any data is received
+        });
+
+        child.stderr.on("data", (data) => {
+	  resolve(data); // Resolve when any data is received
+        });
+
+        child.on("close", (code) => {
+          if (code !== 0) {
+            reject(`Process exited with code ${code}`);
+          }
+        });
+
+        child.on("error", (error) => {
+          reject(`Process error: ${error.message}`);
+        });
       }
-      console.log(`stdout: ${stdout}`);
-      console.error(`stderr: ${stderr}`);
-    });
+    );
   }
 
   initFirstMessage() {
@@ -42,23 +63,29 @@ export class Brain {
 
   async chat(msgs: Message[]) {
     const messages = msgs.length === 0 ? this.buildInitMessages() : msgs;
-    const readline = Readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-    const input = await readline.question(">>> You: ");
-    readline.close();
-    console.log("----");
 
-    const cmsg = new CMessage("user", input);
-    cmsg.save();
-    const userMessage: Message = cmsg.msg;
-    messages.push(userMessage);
+    const lastMsg = messages[messages.length - 1];
+    if (!lastMsg || lastMsg.role !== "tool") {
+      // if the last message is not a toolCall Response, let user input
+      const readline = Readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+      const input = await readline.question(">>> You: ");
+      readline.close();
+      console.log("----");
+
+      const cmsg = new CMessage("user", input);
+      cmsg.save();
+      const userMessage: Message = cmsg.msg;
+      messages.push(userMessage);
+    }
 
     const response = await this.ollama.chat({
-      model: "llama3.1",
+      model: this.model,
       messages: messages,
       stream: true,
+      tools: Object.values(tools),
     });
 
     await stdOutWriteSync(">>> Sisyphus: ");
@@ -69,11 +96,20 @@ export class Brain {
       answer += words;
     }
     console.log("\n----");
+
+    if (checkIfToolCall(answer)) {
+      // Add function response to the conversation
+      const toolCmsg = buildToolCallResponseCMessage(answer);
+      console.debug(toolCmsg)
+      toolCmsg.save();
+      messages.push(toolCmsg.msg);
+      await this.chat(messages);
+    }
+
     const answerCMsg = new CMessage("assistant", answer);
     answerCMsg.save();
     const answerMessage: Message = answerCMsg.msg;
     messages.push(answerMessage);
-
     await this.chat(messages);
   }
 }
