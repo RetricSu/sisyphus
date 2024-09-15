ab9fd13590def1b1d4999f3d35f40896ea77b06a
RetricSu <inneverland2013@gmail.com>
2024-09-16 01:37:18 +0800

    Fix create db path (#5)
    * fix: create-table.sql path
    
    * chore: bump v0.1.0-rc3

--- src/memory/database.ts
+++ src/memory/database.ts
@@ -1,5 +1,6 @@
 import DatabaseConstructor, { type Database } from 'better-sqlite3';
 import fs from 'fs';
+import path from 'path';
 
 export const db: Database = new DatabaseConstructor('my-database.db');
 
@@ -8,7 +9,8 @@ export const createTables = () => {
   // Check if the table exists before executing the SQL
   const tableExists = db.prepare("SELECT name FROM sqlite_master WHERE type='table' AND name='messages';").get();
   if (!tableExists) {
-    const sql = fs.readFileSync('src/memory/create-table.sql', 'utf8');
+    const currentFileFolder = path.dirname(__filename);
+    const sql = fs.readFileSync(path.resolve(currentFileFolder, './create-table.sql'), 'utf8');
     db.exec(sql);
   }
 };
