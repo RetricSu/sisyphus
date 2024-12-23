b17809e34135d12bb3cae7999ce58d289a10c26d
RetricSu <inneverland2013@gmail.com>
2024-12-23 23:35:10 +0800

    feat: color user input in terminal green and output ai first in terminal-bot (#46)
    * feat: color user input in terminal green
    
    * fix: color user input in terminal green and fix extra line
    
    * fix: use require for chalk import
    
    * chore
    
    * use v4 chalk

--- src/agent/terminal-bot.ts
+++ src/agent/terminal-bot.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 import process from 'process';
 import Readline from 'readline/promises';
 import { AMessage } from '../memory/a-message';
@@ -20,13 +21,17 @@ export class TerminalBot extends Agent {
   async chat() {
     await this.loadPromptMessage();
 
+    // call the model first to get the first message
+    await stdOutWriteSync(`>>> ${this.name}: `);
+    await this.call({ requestMsg: { role: 'user', content: 'start' } });
+
     while (true) {
       // if the last message is not a toolCall Response, let user input
       const readline = Readline.createInterface({
         input: process.stdin,
         output: process.stdout,
       });
-      const input = await readline.question('>>> You: ');
+      const input = await readline.question(chalk.green('>>> You: '));
       readline.close();
       console.log('----');
 
