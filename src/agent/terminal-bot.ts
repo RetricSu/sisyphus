50490de57274bb74d0bd946b84bd8fafb3c63746
RetricSu <inneverland2013@gmail.com>
2024-12-23 20:59:31 +0800

    feat: color user input in terminal green

--- src/agent/terminal-bot.ts
+++ src/agent/terminal-bot.ts
@@ -1,5 +1,6 @@
 import process from 'process';
 import Readline from 'readline/promises';
+import chalk from 'chalk';
 import { AMessage } from '../memory/a-message';
 import { Agent } from './base';
 import { stdOutWriteSync } from './util';
@@ -20,12 +21,16 @@ export class TerminalBot extends Agent {
   async chat() {
     await this.loadPromptMessage();
 
+    // call the model first to get the first message
+    await stdOutWriteSync(`>>> ${this.name}: `);
+    await this.call({requestMsg: {role: 'user', content: 'start'}});
+
     while (true) {
       // if the last message is not a toolCall Response, let user input
       const readline = Readline.createInterface({
         input: process.stdin,
         output: process.stdout,
-      });
+      const input = await readline.question(chalk.green(\'>>> You: \'));
       const input = await readline.question('>>> You: ');
       readline.close();
       console.log('----');
