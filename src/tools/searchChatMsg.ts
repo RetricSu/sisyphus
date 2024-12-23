dc20c5ee5ca0d3bc3e544de5bc0acf6e19a88348
RetricSu <inneverland2013@gmail.com>
2024-12-23 17:36:02 +0800

    wip (#43)

--- src/tools/searchChatMsg.ts
+++ src/tools/searchChatMsg.ts
@@ -4,25 +4,28 @@ import { Memory, type MemoryMetadata } from '../memory/long-term';
 import { PromptFile } from '../prompt';
 import type { Tool, ToolBox } from './type';
 
-export interface MemoryToolExecParameter {
+export interface SearchChatMsgToolExecParameter {
   text: string;
 }
 
-export type MemoryToolBoxType = ToolBox<[MemoryToolExecParameter], Promise<(Message & { created_at: string })[]>>;
+export type SearchChatMsgToolBoxType = ToolBox<
+  [SearchChatMsgToolExecParameter],
+  Promise<(Message & { created_at: string })[]>
+>;
 
-export function buildMemoryToolBox(memoId: string) {
-  const memoryToolBox: MemoryToolBoxType = {
+export function buildChatMessageSearchToolBox(memoId: string) {
+  const searchChatMsgToolBox: SearchChatMsgToolBoxType = {
     fi: {
       type: 'function',
       function: {
-        name: 'search_memory',
-        description: 'Search the remembered and maybe-related content from the persistent memory',
+        name: 'search_chat_messages',
+        description: 'Search the remembered and maybe-related content from the persistent chat message database',
         parameters: {
           type: 'object',
           properties: {
             text: {
               type: 'string',
-              description: 'The text to search in the memory',
+              description: 'The text to search in the chat-messages',
             },
           },
           required: ['text'],
@@ -30,9 +33,9 @@ export function buildMemoryToolBox(memoId: string) {
       },
     },
     params: z.object({
-      text: z.string().describe('The text to search in the memory'),
+      text: z.string().describe('The text to search in the chat-messages'),
     }),
-    exec: async ({ text }: MemoryToolExecParameter) => {
+    exec: async ({ text }: SearchChatMsgToolExecParameter) => {
       const memory = new Memory(memoId);
       const results = await memory.query({ searchString: text, limit: 5 });
       const msg: (Message & { created_at: string })[] = [];
@@ -50,14 +53,14 @@ export function buildMemoryToolBox(memoId: string) {
       return msg;
     },
   };
-  return memoryToolBox;
+  return searchChatMsgToolBox;
 }
 
 const tool: Tool = {
-  names: ['search_memory'],
+  names: ['search_chat_messages'],
   build: (p: PromptFile) => {
     const memoId = p.memoId;
-    return [buildMemoryToolBox(memoId)];
+    return [buildChatMessageSearchToolBox(memoId)];
   },
 };
 
