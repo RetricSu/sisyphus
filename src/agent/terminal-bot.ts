a25e62e234c947bab80f402eef78333fc0643ef4
RetricSu <inneverland2013@gmail.com>
2024-12-25 15:26:48 +0800

    add loader for terminal bot

--- src/agent/terminal-bot.ts
+++ src/agent/terminal-bot.ts
@@ -1,32 +1,66 @@
+import { AssistantContent, CoreMessage } from 'ai';
 import chalk from 'chalk';
+import ora from 'ora';
 import process from 'process';
 import Readline from 'readline/promises';
 import { AMessage } from '../memory/a-message';
 import { Agent } from './base';
-import { stdOutWriteSync } from './util';
 
 export class TerminalBot extends Agent {
   pipeResponse?: ((name: string, word: string) => any) | undefined;
-  constructor({ saveMemory, promptName }: { saveMemory?: boolean; promptName?: string }) {
-    const pipeResponse = async (_name: string, word: string) => {
-      return await console.log(word);
-    };
+  constructor({
+    saveMemory,
+    promptName,
+  }: {
+    saveMemory?: boolean;
+    promptName?: string;
+  }) {
     super({
       saveMemory,
       promptName,
-      pipeResponse,
     });
+  }
+
+  displayAssistantMessage(msgs: CoreMessage[]) {
+    for (const msg of msgs.filter((m) => m.role === 'assistant')) {
+      let content = '';
+      const assistantMsgContent = msg.content as AssistantContent;
+      if (typeof assistantMsgContent === 'string' && assistantMsgContent !== '') {
+        content = assistantMsgContent;
+      } else {
+        if (Array.isArray(assistantMsgContent)) {
+          // don't display tool-calls
+          content += assistantMsgContent.filter((c) => c.type === 'text').map((c) => c.text);
+        } else {
+          content = JSON.stringify(assistantMsgContent);
+        }
+      }
+      if (content !== '') {
+        console.log(`>>> ${this.name}: ${content}`);
+      }
+    }
+  }
+
+  private async reqMsgsFromAI(requestMsg?: { role: string; content: string }) {
+    const loader = ora(chalk.blue(`${this.name} is thinking..`)).start();
+    try {
+      const msgs = await this.call({
+        requestMsg,
+      });
+      loader.stop();
+      this.displayAssistantMessage(msgs);
+    } catch (error) {
+      loader.fail(`${this.name}: Error ${error}`);
+    }
   }
 
   async chat() {
     await this.loadPromptMessage();
 
     // call the model first to get the first message
-    await stdOutWriteSync(`>>> ${this.name}: `);
-    await this.call({ requestMsg: { role: 'user', content: 'start' } });
+    await this.reqMsgsFromAI({ role: 'user', content: 'start' });
 
     while (true) {
-      // if the last message is not a toolCall Response, let user input
       const readline = Readline.createInterface({
         input: process.stdin,
         output: process.stdout,
@@ -35,9 +69,8 @@ export class TerminalBot extends Agent {
       readline.close();
       console.log('----');
 
-      await stdOutWriteSync(`>>> ${this.name}: `);
       const amsg = new AMessage(this.memoId, 'user', input);
-      await this.call({ requestMsg: amsg.msg });
+      await this.reqMsgsFromAI(amsg.msg);
     }
   }
 }
